6f91cb7613a7ca81d4841a43ad7f525374003c59
trungkien <trungkien7300@gmail.com>
2021-03-26 22:20:30 +0700

    use DOM and JQuery to code

---
+++ app.js
@@ -0,0 +1,190 @@
+// -----------------------------JQuery used to code------------------------
+
+$(document).ready(function(){
+  const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+      id: 10,
+      title: "bison steak",
+      category: "dinner",
+      price: 22.99,
+      img: "./images/item-10.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
+  function displayall_J(){
+    let temp = "";
+    for(let i=0;i<menu.length;i++)
+    {
+      temp+=`<div class="item">
+      <img src="${menu[i].img}" alt="">
+      <div class="content">
+          <h3>${menu[i].title} <span>${menu[i].price}</span></h3>
+          <p>${menu[i].desc}</p>
+      </div>
+      </div>`
+    }
+    $(".items").html(temp) ; // maybe use append method for this situation
+  }
+  function display_categories_J(cate){
+    let temp = "";
+    for(let i=0;i<menu.length;i++)
+    {
+      if(menu[i].category===cate)
+      temp+=`<div class="item">
+      <img src="${menu[i].img}" alt="">
+      <div class="content">
+          <h3>${menu[i].title} <span>${menu[i].price}</span></h3>
+          <p>${menu[i].desc}</p>
+      </div>
+      </div>`
+    }
+    $(".items").html(temp) ;
+  }
+  
+  displayall_J();
+
+  $("#all").click(function(){
+    displayall_J();
+  });
+  $("#breakfast").click(function(){
+    display_categories_J("breakfast");
+  });
+  $("#lunch").click(function(){
+    display_categories_J("lunch");
+  });
+  $("#shakes").click(function(){
+    display_categories_J("shakes");
+  });
+  $("#dinner").click(function(){
+    display_categories_J("dinner");
+  });
+})
+// -----------------------DOM used to code----------------------------------
+
+// const items = document.querySelector(".items");
+// const all_btn = document.getElementById("all");
+// const breakfast_btn = document.getElementById("breakfast");
+// const lunch_btn = document.getElementById("lunch");
+// const shakes_btn = document.getElementById("shakes");
+// const dinner_btn = document.getElementById("dinner");
+
+// function displayall (){
+//     items.innerHTML = "";
+//     for(let i=0;i<menu.length;i++){
+//      items.innerHTML += `<div class="item">
+//     <img src="${menu[i].img}" alt="">
+//     <div class="content">
+//         <h3>${menu[i].title} <span>${menu[i].price}</span></h3>
+//         <p>${menu[i].desc}</p>
+//     </div>
+//     </div>`;
+//     }
+// }
+// function display_categories(cate){
+//     items.innerHTML="";
+//     for(let i=0;i<menu.length;i++)
+//     {
+//         if(menu[i].category === cate)
+//         {
+//             items.innerHTML += `<div class="item">
+//             <img src="${menu[i].img}" alt="">
+//             <div class="content">
+//                 <h3>${menu[i].title} <span>${menu[i].price}</span></h3>
+//                 <p>${menu[i].desc}</p>
+//             </div>
+//             </div>`;
+//         }
+//     }
+// }
+// window.addEventListener("DOMContentLoaded",function(){
+//     displayall();
+// });
+// all_btn.onclick = function(){
+//     displayall();
+// }
+// breakfast_btn.onclick = function()
+// {
+//     display_categories("breakfast");
+// }
+// shakes_btn.onclick = function(){
+//     display_categories("shakes");
+// }
+
+// lunch_btn.onclick = function(){
+//     display_categories("lunch");
+// }
+// dinner_btn.onclick = function(){
+//     display_categories("dinner");
+// }
